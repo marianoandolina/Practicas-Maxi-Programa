d79cc28af0f2273a92bc8547784603f02603aa8e
marianoandolina <109903560+marianoandolina@users.noreply.github.com>
2022-09-04 20:07:21 -0300

    Add files via upload

---
+++ Resolucion Ejercicios 1.py
@@ -0,0 +1,126 @@
+# Ejercicio nro 2
+''' Hacer	un	programa	para	solicitar	por	teclado	un	número	y	luego	devolver	su	
+valor	elevado	al	cubo. '''	
+
+# n1 = int(input("Ingresa un numero: "))
+# r = n1 * n1 * n1
+# print(f"El resultado del numero elevado al cubo es {r}")
+
+# Ejercicio nro 3
+''' Hacer un programa que permita ingresar el año actual y el año de la	fecha	de	
+nacimiento	de	una	persona	y	luego	calcule	y emita	por	pantalla	su	edad. '''
+
+# aa = int(input("Ingresa el año actual: "))
+# an = int(input("Ingresa el año de tu nacimiento: "))
+# e = aa - an
+# print(f"Tu edad es {e} años")
+
+# Ejercicio nro 4
+
+'''Hacer	un	programa	que	permita	ingresar	los	kilómetros	existentes	entre	dos	
+ciudades	y	la	velocidad	promedio	de	un	vehículo.	Calcular	y	emitir	por	pantalla	
+el	tiempo	aproximado	que	demandará	llegar	de	un	punto	a	otro	teniendo	en	
+cuenta	los	datos	ingresados.'''	
+
+# k = int(input("Ingrese la distancia entre las ciudades: "))
+# v = int(input("Ingrese la velocidad promedio de viaje: "))
+# t= k / v
+# print(f"El tiempo estimados de viaje es de {t} hs.")
+
+# Ejercicio nro 5
+'''Una	casa	de	computación	paga	a	sus	empleados	un	sueldo	fijo	de	ARS15000	
+más	una	comisión	del	5%	sobre	el	total	facturado	por	cada	empleado.	Hacer	un	
+programa	para	ingresar	el	total	facturado	por	un	empleado	y	que	luego	calcule	
+y	emita	por	pantalla	el	sueldo	total	a	cobrar	por	el	mismo.'''
+
+# tf= int(input("Ingrese el total facturado por la empresa: "))
+# c= (5 * tf) / 100
+# sueldo= 15000 + c
+# print(f"El sueldo final incluida la comision este mes es de {sueldo}")
+
+# Ejercicio nro 6
+'''Hacer	un	programa	para	ingresar	por	teclado	las	tres	notas	de	exámenes	de	un	
+alumno	y	luego	calcule	y	emita	por	pantalla	el	promedio	final.'''
+
+# n1= int(input("Ingrese la nota 1: "))
+# n2= int(input("Ingrese la nota 2: "))
+# n3= int(input("Ingrese la nota 3: "))
+
+# promedio= (n1 + n2 + n3) / 3
+# print(f"El promedio de las 3 notas ingresadas es de {promedio}")
+
+# Ejercicio nro 7
+'''Hacer	un	programa	para	ingresar	por	teclado	los	metros	cuadrados	totales	de	
+un	predio	y	los	metros	cuadrados	cubiertos;	luego	calcular	y	mostrar	por	
+pantalla	el	porcentaje	de	metros	cuadrados	cubiertos	y	el	porcentaje	de	
+metros	cuadrados	descubiertos.'''
+
+# mt= int(input("Ingrese los metros cuadrados totales del predio: "))
+# mc= int(input("Ingrese los metros cubiertos totales del predio: "))
+
+# pc= (mc*100) / mt
+# md= mt - mc
+# pd= (md * 100) / mt
+# print(f"El promedio de los metros cubiertos es de {pc}")
+# print(f"El promedio de los metros descubiertos es de {pd}")
+
+# Ejercicio nro 8
+'''Una	importante	cadena	de	delivery	cuenta	con	una	promoción	por	tiempo	
+limitado	en	la	que	otorga	un	15%	de	descuento	sobre	el	total	del	valor	de	la	
+compra	realizada.	Hacer	un	programa	para	solicitar	el	monto	total	y	el	mismo	
+calcule	y	emita	por	pantalla	el	total	a	cobrar	con	el	descuento	aplicado.'''	
+
+# mt= int(input("Ingresa el monto total de la compra: "))
+# precio_final= mt * 0.85
+# print(f"El precio a pagar luego de aplicado el descuento es de {precio_final}")
+
+# Ejercicio nro 9
+'''Una	universidad	desea	conocer	los	porcentajes	de	mujeres	y	hombres	en	las	
+carreras	de	ciencias	exactas.	Se	solicita	un	programa	para	cargar	la	cantidad	de	
+mujeres	y	la	cantidad	de	hombres	y	que	el	mismo	calcule	y	emita	por	pantalla	
+los	porcentajes	correspondientes.'''
+
+# hombres= int(input("Ingrese la cantidad de hombres que asisten a la clase: "))
+# mujeres= int(input("Ingrese la cantidad de mujeres que asisten a la clase: "))
+# total_alumnos= hombres + mujeres
+
+# porcentaje_hombres= (hombres * 100) / total_alumnos
+# porcentaje_mujeres= (mujeres * 100) / total_alumnos
+
+# print(f"El porcentaje de hombres en la clase es de {porcentaje_hombres}")
+# print(f"El porcentaje de mujeres en la clase es de {porcentaje_mujeres}")
+
+# Ejercicio nro 10
+""" Hacer	un	programa	que	permita	ingresar	por	teclado	dos	números	y	que	luego	
+muestre	por	pantalla	la	suma,	la	resta,	la	multiplicación	y	la	división	de	dichos	
+números.	Se	deben	mostrar	cuatro	resultados	en	pantalla.	Los	números	deben	
+ser	solicitados	una	única	vez. """	
+
+# n1= int(input("Ingrese el primer numero: "))
+# n2= int(input("Ingrese el segundo numero: "))
+
+# s= n1 +n2
+# r= n1 - n2
+# mult= n1 * n2
+# div= n1 / n2
+
+# print(f"La suma da {s}, la resta da {r}, la multiplicacion da {mult} y la divicion da {div}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Resolucion Ejercicios 2 if.py
@@ -0,0 +1,22 @@
+#Ejercicio 1
+# Ingresamos un programa que nos diga si la persona es mayor de edad. En este caso 21 años.
+
+# edad= int(input("Ingrese la edad: "))
+# if edad >= 21:
+#     print("La persona es mayor de edad")
+# else:
+#     print("La persona es menor de edad")
+
+#Ejercicio 2
+
+# print("A continuacion le pedimos que ingrese dos numeros distintos.")
+# n1= int(input("Ingrese el primer numero: "))
+# n2= int(input("Ingrese el segundo numero, (recuerde qe sea distinto al primero): "))
+
+# if n1>n2:
+#     r= n1 - n2
+#     print(f"El resultado de la resta de el mayor con el menor es {r}")
+# else:
+#     r= n2 - n1
+#     print(f"El resultado de la resta de el mayor con el menor es {r}")
+
