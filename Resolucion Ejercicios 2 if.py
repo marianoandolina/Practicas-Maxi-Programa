06bac59cbdae32be971264e75b7ad6936ce16955
Mariano Andolina <mariano.3gs@gmail.com>
2022-09-05 21:07:51 -0300

    5/9 (2)

--- Resolucion Ejercicios 2 if.py
+++ Resolucion Ejercicios 2 if.py
@@ -1,4 +1,5 @@
-#Ejercicio 1
+# PREEJERCICIO 1
+
 # Ingresamos un programa que nos diga si la persona es mayor de edad. En este caso 21 años.
 
 # edad= int(input("Ingrese la edad: "))
@@ -7,7 +8,7 @@
 # else:
 #     print("La persona es menor de edad")
 
-#Ejercicio 2
+#PREEJERCICIO 2
 
 # print("A continuacion le pedimos que ingrese dos numeros distintos.")
 # n1= int(input("Ingrese el primer numero: "))
@@ -23,9 +24,34 @@
 # Hacer	un	programa	para	ingresar	un	número	y	luego	se	emita	por	pantalla	un	
 # cartel	aclaratorio	si	“Es	mayor	a	10”	o	“No	es	mayor	a	10”.
 
-n1= int(input("Ingrese un numero: "))
+# n1= int(input("Ingrese un numero: "))
 
-if n1>10:
-    print("El numero es mayor a 10")
-else:
-    print("No es mayor a 10")
+# if n1>10:
+#     print("El numero es mayor a 10")
+# else:
+#     print("No es mayor a 10")
+
+# Hacer	un	programa	para	ingresar	dos	números	distintos	y	luego	se	muestre	por	
+# pantalla	el	menor	de	ellos.	
+
+# n1= int(input("Ingresa un numero: "))
+# n2= int(input("Ingresa otro numero distinto al anterior: "))
+
+# if n1<n2:
+#     print(n1)
+# else:
+#     print(n2)    3
+
+#  Hacer	un	programa	para	ingresar	dos	números	y	que	luego	emita	por	pantalla	
+# el	mayor	de	ellos	o	un	cartel	aclaratorio	“Son	iguales”	en	el	caso	de	que	así	sea.
+
+# n1= int(input("Ingrese un numero: "))
+# n2= int(input("Ingrese otro numero: "))
+
+# if n1>n2:
+#     print(n1)
+# elif n1<n2:
+#     print(n2)
+# if n1==n2:
+#     print("Son Iguales")
+
